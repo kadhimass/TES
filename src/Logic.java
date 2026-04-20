bd42f49c87396d94e07d3d89594736757459ce05
Rendy Syahputra Riyadi <152749994+Rendyprobe@users.noreply.github.com>
2026-04-20 22:25:38 +0700

    fix bux :

--- src/Logic.java
+++ src/Logic.java
@@ -1,3 +1,16 @@
-public class Logic {
+import java.util.Scanner;
 
+public class GanjilGenap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+
+        // Logika IF-ELSE
+        if (angka % 2 == 0) {
+            System.out.println(angka + " adalah angka Genap.");
+        } else {
+            System.out.println(angka + " adalah angka Ganjil.");
+        }
+    }
 }
